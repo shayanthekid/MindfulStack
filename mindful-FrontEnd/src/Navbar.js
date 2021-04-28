67b02587dbb412d3e04374563b55b599167e63eb
shayanthekid <sajidfayazhaniff@gmail.com>
2021-04-28 18:00:14 +0530

    Navbar completed

--- mindful-FrontEnd/src/Navbar.js
+++ mindful-FrontEnd/src/Navbar.js
@@ -1,17 +1,42 @@
 import {Link} from 'react-router-dom';
+import {useState,useEffect } from 'react';
+import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies';
 
 
 const Navbar = () => {
+const [cookie,setCookie] = useState(false);
+    const cookie_key = 'LoggedIn';
+
+    const loggout= ()=>{
+
+        delete_cookie(cookie_key);
+        window.location.href='/';
+};
+
+ useEffect(()=>{
+  
+if(read_cookie(cookie_key) == ''){
+    setCookie(false);
+}
+else{
+    setCookie(true);
+}
+ });
     return (
        <nav className="navbar">
            <h1>Mindful Blog</h1>
            <div className="links">
                <Link to="/">Home</Link>
-               <Link to="/login" style={{
+             {!cookie &&  <Link to="/login" style={{
                    color:"white",
                    backgroundColor: "#db324d",
                    borderRadius: "8px"
-               }} >Login</Link>
+               }} >Login</Link>}
+              {cookie && <Link onClick={loggout} to="/" style={{
+                   color:"white",
+                   backgroundColor: "#417b5a",
+                   borderRadius: "8px"
+               }} >Logout</Link>}
            </div>
        </nav>
       );
