1e8d1d24a677acf2f8b5b061bd3516d1f4fcc810
shayanthekid <sajidfayazhaniff@gmail.com>
2021-04-28 21:54:18 +0530

    grid problem

--- mindful-FrontEnd/src/BlogList.js
+++ mindful-FrontEnd/src/BlogList.js
@@ -4,25 +4,37 @@ const BlogList = ({blogs}) => {
 
         <div className="blog-list">
           <h1>All blogs</h1>
+        
               {blogs.map(blog => (
-        <div className="blog-preview" key={blog._id} >
+        <div className="cards" key={blog._id} >
           {/* <img src={`./${blog.image}.jpg`} alt="An Image"/>
           
           <h2>{ blog.title }</h2>
           <p>{ blog.date }</p>
           <p>{ blog.price }</p>
           <p>{ blog.seats }</p> */}
-        <div className="card">
-          <div className="car-image">
+        {/* <div className="card">
+         <img src="./image1.jpg" className="card-image" alt=""/>
             <h2>Title</h2>
             <p>Text</p>
-          </div>
+          
+          
+        </div> */}
+       
+         <div className="card"></div>
+       <div className="container">
+           <img src="./image1.jpg" className="card-image" alt=""/>
+        <div className="details">
+          <h3>poop</h3> 
+          <p>sdfdfsdfsdfsdf</p>
         </div>
+       </div>
 
         </div>
+        
       ))}
-        </div>
-
+      
+</div>
      );
 }
  
