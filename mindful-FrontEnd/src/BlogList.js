a548273dc0515515a4d1da3518d699aca0108bf9
shayanthekid <sajidfayazhaniff@gmail.com>
2021-04-28 20:14:40 +0530

    Blogs Show feature complete

--- mindful-FrontEnd/src/BlogList.js
+++ mindful-FrontEnd/src/BlogList.js
@@ -3,6 +3,7 @@ const BlogList = ({blogs}) => {
     return ( 
 
         <div className="blog-list">
+          <h1>All blogs</h1>
               {blogs.map(blog => (
         <div className="blog-preview" key={blog._id} >
           <h2>{ blog.title }</h2>
--- mindful-FrontEnd/src/Home.js
+++ mindful-FrontEnd/src/Home.js
@@ -1,15 +1,16 @@
 import { useState,useEffect } from 'react';
 import BlogList from './BlogList';
-
+import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies';
 
 const Home = () => {
 const [blogs,setBlogs] = useState(null);
 const [isPending,setIsPending]= useState(true);
 const [error,setError]= useState(null);
+const [cookie,setCookie] = useState(false);
+const cookie_key = 'LoggedIn';
 
 useEffect(()=>{
     const abortConst = new AbortController();
-
 
 
     fetch('http://localhost:3000/all-events',{
@@ -25,9 +26,20 @@ useEffect(()=>{
        return res.json()
     })
     .then((data)=>{
+        
+if(read_cookie(cookie_key) == ''){
+    setCookie(false);
+    setIsPending(false);
+    setBlogs(null);
+}
+else{
+    setCookie(true);
     setBlogs(data);
     setIsPending(false);
     setError(null);
+}
+    
+
     })
     .catch((err)=>{
     if(err.name ==="AbortError"){
@@ -48,9 +60,12 @@ console.log("Fetch Aborted");
 
 return (
     <div className="home">
+        {!cookie &&   <h1>Please Login to view blogs</h1>}
+       
+        
         {error && <div> {error} </div>}
-    {isPending && <div> Loading.... </div>}
-    {blogs && <BlogList blogs={blogs} />}
+        {isPending && <div> Loading.... </div>}
+        {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 }
